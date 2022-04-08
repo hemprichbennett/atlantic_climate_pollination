df99f21ce0e5b91c3316939e7f19e8658a1e4cc7
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-04-08 14:28:59 +0100

    created optimized script

--- optimized.R
+++ optimized.R
@@ -45,63 +45,15 @@ thin_function <- function(chosen_sp, thin_dist){
 
 # thinning records by 5 km
 
-five_list <- lapply()
+five_list <- lapply(spp, function(x) thin_function(x, thin_dist = 5))
 
-clean_df_thin_5 <- rbindlist(thin_5)
+clean_df_thin_5 <- bind_rows(five_list)
 
-
-# thinning records by 10 km
-thin_10 <- list()
-for(i in 1:length(spp)){
-  df <- clean_df %>%
-    filter(species %in% spp[i])
-  thinned <- thin(df,
-                  lat.col = "lat",
-                  long.col = "lon",
-                  spec.col = "species",
-                  thin.par = 10, # distance in km
-                  reps = 1,
-                  locs.thinned.list.return = TRUE,
-                  write.files = FALSE,
-                  write.log.file = FALSE)
-  thin_10[[i]] <- data.frame(species = rep(spp[i], nrow(thinned[[1]])),
-                             lon = thinned[[1]]$Longitude,
-                             lat = thinned[[1]]$Latitude)
-}
-clean_df_thin_10 <- rbindlist(thin_10)
-
-
-# Check thinned records
-ggplot() +
-  borders("world", colour="gray50", fill="gray50") +
-  geom_point(data = clean_df_thin_5, aes(x = lon, y = lat),
-             colour = "blue", size = 1.5) +
-  geom_point(data = clean_df_thin_10, aes(x = lon, y = lat),
-             colour = "red", size = 1.0) +
-  coord_sf(xlim = c(-160, -28), ylim = c(-60,90)) +
-  theme_bw()
-
-
-# counting records by species
-n_5 <- clean_df_thin_5 %>%
-  group_by(species) %>%
-  summarize(n_thin_5 = n())
-
-n_10 <- clean_df_thin_10 %>%
-  group_by(species) %>%
-  summarize(n_thin_10 = n())
-
-
-# adding counts to the n_records table
-n_records <- read_csv("outputs/02_n_records.csv")
-
-n_records <- n_records %>%
-  left_join(n_5, by = "species") %>%
-  left_join(n_10, by = "species") %>%
-  replace_na(list(n_thin_5 = 0, n_thin_10 = 0))
-
-
-# writing outputs
-write_csv(n_records, path = "outputs/03_n_thinned_records.csv")
 write_csv(clean_df_thin_5, path = "outputs/03_clean_df_thin_5.csv")
+
+# and now 10 km
+ten_list <- lapply(spp, function(x) thin_function(x, thin_dist = 10))
+
+clean_df_thin_10 <- bind_rows(ten_list)
+
 write_csv(clean_df_thin_10, path = "outputs/03_clean_df_thin_10.csv")

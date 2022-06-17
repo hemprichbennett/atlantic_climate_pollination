44c83572b73f195514ddda538ec5f6dc46918918
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-06-17 13:34:09 +0100

    Reworked the occurrence download so that it's a single gbif query. Removed specieslink steps, so they can be done in a separate script

---
+++ scripts/1_download_occ.R
@@ -0,0 +1,62 @@
+  # Script was done by
+  # Bruno M. Carvalho (https://github.com/brunomc-eco)
+  
+  # Script was edited by
+  # Luara Tourinho
+  
+  # Date: 01 abr 2021
+
+library(devtools)
+library(tidyverse)
+library(rgbif)
+library(taxize) # for get_gbifid_
+library(data.table)
+library(dplyr)
+library(purrr)
+library(readr)
+library(magrittr) # for %T>% pipe
+library(rgbif) # for occ_download
+library(Rocc) # fpr spescieslink
+library(bit64)
+
+
+# Creating/reading our species list
+# reading the data in will give a warning, because some rows have more columns
+# for the source reference than others
+interaction_file <- read_csv("./data/raw_data/interaction_list.csv")
+
+splist <- unique(c(interaction_file$Pollinator, interaction_file$Plant))
+
+
+# GBIF =--------------------------------------------------------------------
+
+
+# getting records from gbif
+# got this code from https://data-blog.gbif.org/post/downloading-long-species-lists-on-gbif/
+# Manual of rgbif: https://cran.r-project.org/web/packages/rgbif/rgbif.pdf
+
+gbif_taxon_keys <- splist %>%
+  get_gbifid_(method="backbone") %>% # get taxonkeys for each species name
+  imap(~ .x %>% mutate(original_sciname = .y)) %>% # add original name back to data.frame
+  bind_rows() # combine all results in a single data.frame
+
+only_keys <- gbif_taxon_keys %>%
+  filter(matchtype == "EXACT" & status == "ACCEPTED") %>% # get only accepted names
+  pull(usagekey) #retain only the taxonkeys
+
+# download data directly at GBIF
+# (file needs to be mannualy fetched at the user's downloads page at gbif.org)
+
+# Do you need a login on GBIF
+# enter GBIF credentials
+credential_df <- read_csv('my_credentials.csv')
+
+temp <- occ_download(
+  pred_in("taxonKey", only_keys),
+  pred("hasCoordinate", TRUE),
+  pred("hasGeospatialIssue", FALSE),
+  format = "SIMPLE_CSV",
+  user = credential_df$username, 
+  pwd = credential_df$password, 
+  email = credential_df$email
+)

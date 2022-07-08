ef07f0261e8a8df0ae66a9fe77da16ffaee614fc
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-07-08 16:16:08 +0100

    Increased number of iterations

--- scripts/in_3_thinning.sh
+++ scripts/in_3_thinning.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=thinning # the name for the cluster scheduler
 #SBATCH --time=01:30:00 # Maximum allowed runtime per iteration
-#SBATCH --array=1-722 # the number of iterations
+#SBATCH --array=1-755 # the number of iterations
 #SBATCH --output=output_messages/thinning%A_%a.out # the name of the output files
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=david.hemprich-bennett@zoo.ox.ac.uk

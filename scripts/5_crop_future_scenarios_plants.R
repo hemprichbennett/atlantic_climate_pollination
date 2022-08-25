335f6786235dc28d0ba3279c2e485b157ca11584
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-08-25 11:32:24 +0100

    Tiagos edits

--- scripts/5_crop_future_scenarios_plants.R
+++ scripts/5_crop_future_scenarios_plants.R
@@ -25,11 +25,11 @@ library(rgdal)
 # And selected variables
 # By Spearman test (0.6): 
 # "X_wc2.1_2.5m_bio_15" "X_wc2.1_2.5m_bio_18" 
-# "X_wc2.1_2.5m_bio_3"   "X_wc2.1_2.5m_bio_8"
+# "X_wc2.1_2.5m_bio_2" "X_wc2.1_2.5m_bio_3"   "X_wc2.1_2.5m_bio_8"
 
 ## Future variables ------------------------------------------------------
 
-to_crop <- raster('./data/processed_data/env_cropped/present/plants/_wc2.1_5m_bio_2.tif')
+to_crop <- raster('./data/processed_data/env_cropped/present/_wc2.1_5m_bio_2.tif')
 #to_crop <- envi.mask2[[1]]
 
 # If future biovariables come separately, build a list like you did before
@@ -45,22 +45,37 @@ to_crop <- raster('./data/processed_data/env_cropped/present/plants/_wc2.1_5m_bi
 
 # 2041-2060
 
-future_var <- stack("./data/raw_data/env/future/wc2.1_5m_bioc_BCC-CSM2-MR_ssp585_2061-2080.tif")
-#future_var <- stack("./data/raw_data/env/future/wc2.1_5m_bioc_BCC-CSM2-MR_ssp245_2061-2080.tif")
+future_var <- stack("./data/raw_data/env/future/wc2.1_5m_bioc_BCC-CSM2-MR_ssp245_2061-2080.tif")
+#future_var <- stack("./data/raw_data/env/future/wc2.1_5m_bioc_BCC-CSM2-MR_ssp585_2061-2080.tif")
 
 # Chose the variables that you selected in "5_crop_environmental_variables.R"
-future_var_stk <- future_var[[c(2,5,7,8,14,15,18)]]
+future_var_stk <- future_var[[c(2,3,8,15,18)]]
 envi_fut.cut<- crop(future_var_stk, to_crop)
 envi_fut.mask<- mask(envi_fut.cut, to_crop)
 
 # Save only that selected variables 5_crop_environmental_variables.R step
-names(envi_fut.mask) <- c("bio_2","bio_5","bio_7","bio_8", "bio_14", "bio_15","bio_18")
-#dir.create(paste0("./data/processed_data/env_sel/future/RCP45/"))
-writeRaster(envi_fut.mask, filename='./data/processed_data/env_sel/future/plants/RCP85/', 
+names(envi_fut.mask) <- c("bio_2","bio_3","bio_8", "bio_15","bio_18")
+dir.create(paste0("./data/processed_data/env_sel/future/RCP45/"))
+dir.create(paste0("./data/processed_data/env_sel/future/RCP85/"))
+writeRaster(envi_fut.mask, filename='./data/processed_data/env_sel/future/RCP45/', 
+            format="GTiff", bylayer=TRUE, suffix="names", overwrite=TRUE)
+writeRaster(envi_fut.mask, filename='./data/processed_data/env_sel/future/RCP85/', 
             format="GTiff", bylayer=TRUE, suffix="names", overwrite=TRUE)
 
-# writeRaster(envi_fut.mask, filename='./data/processed_data/env_sel/future/plants/RCP45/', 
-#            format="GTiff", bylayer=TRUE, suffix="names", overwrite=TRUE)
+
+# However, that write function above is adding a "_" before the raster names
+# because that I am saving one by one
+
+# Saving one by one
+#writeRaster(envi_fut.mask[[1]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_3', 
+#            format="GTiff", overwrite=TRUE)
+#writeRaster(envi_fut.mask[[2]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_8', 
+#            format="GTiff", overwrite=TRUE)
+#writeRaster(envi_fut.mask[[3]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_15', 
+#            format="GTiff", overwrite=TRUE)
+#writeRaster(envi_fut.mask[[4]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_18', 
+#            format="GTiff", overwrite=TRUE)
+
 
 
 # Repeat this routine for other scenarios, GCMs, and years that interest you

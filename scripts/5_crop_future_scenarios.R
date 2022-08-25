1fc0284995ddc0ebf7f751b0f8adad730a7e2207
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-08-25 15:37:46 +0100

    Moved the future file cropping into a loop for the two scenarios

--- scripts/5_crop_future_scenarios.R
+++ scripts/5_crop_future_scenarios.R
@@ -35,7 +35,7 @@ raster_paths_to_use <- raster_files[
 ]
 
 crop_by <- stack(raster_paths_to_use)
-#crop_by <- envi.mask2[[1]]
+
 
 
 # If future biovariables come separately, build a list like you did before
@@ -45,43 +45,38 @@ crop_by <- stack(raster_paths_to_use)
 
 # spp126 ------------------------------------------------------------------
 
-# Ps. 2021-2040 was done in the step "5_crop_environmental_variables.R"
 
 # Choosing another future scenario
 
-# 2041-2060
+future_files <- list.files('./data/raw_data/env/future',
+                           full.names = T)
+base_dir <- 'data/processed_data/env_sel/future/'
+if(!dir.exists(base_dir)){
+  dir.create(base_dir)
+}
 
-future_var <- stack("./data/raw_data/env/future/wc2.1_5m_bioc_BCC-CSM2-MR_ssp245_2061-2080.tif")
-#future_var <- stack("./data/raw_data/env/future/wc2.1_5m_bioc_BCC-CSM2-MR_ssp585_2061-2080.tif")
+for(f in future_files){
+  print(f)
+  # get the climate scenario of the file, format it nicely
+  scenario <- gsub('.+ssp|_2061.+', '', f)
+  scenario <- sub('.', 'RCP', scenario)
+  
+  # make the output directory, if it doesn't already exist
+  out_dir <- paste0(base_dir, scenario)
+  if(!dir.exists(out_dir)){dir.create(out_dir)}
+  
+  # load in the data
+  future_var <- stack(f)
+  # select only our desired variables
+  future_var_stk <- future_var[[desired_variables]]
+  # crop them by our selected present variables
+  envi_fut.cut<- crop(future_var_stk, crop_by)
+  
+  # rename the variables for simplicity
+  names(envi_fut.cut) <- paste0('bio_', gsub('.+\\.', '', names(envi_fut.cut)))
+  
+  # save the raster
+  writeRaster(envi_fut.cut, filename=paste0(out_dir, '/'),
+              format="GTiff", bylayer=TRUE, suffix="names", overwrite=TRUE)
+}
 
-# Chose the variables that you selected in "5_crop_environmental_variables.R"
-future_var_stk <- future_var[[desired_variables]]
-envi_fut.cut<- crop(future_var_stk, crop_by)
-envi_fut.mask<- mask(future_var_stk, crop_by)
-
-# Save only that selected variables 5_crop_environmental_variables.R step
-names(envi_fut.mask) <- c("bio_2","bio_3","bio_8", "bio_15","bio_18")
-dir.create(paste0("./data/processed_data/env_sel/future/RCP45/"))
-dir.create(paste0("./data/processed_data/env_sel/future/RCP85/"))
-writeRaster(envi_fut.mask, filename='./data/processed_data/env_sel/future/RCP45/', 
-            format="GTiff", bylayer=TRUE, suffix="names", overwrite=TRUE)
-writeRaster(envi_fut.mask, filename='./data/processed_data/env_sel/future/RCP85/', 
-            format="GTiff", bylayer=TRUE, suffix="names", overwrite=TRUE)
-
-
-# However, that write function above is adding a "_" before the raster names
-# because that I am saving one by one
-
-# Saving one by one
-#writeRaster(envi_fut.mask[[1]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_3', 
-#            format="GTiff", overwrite=TRUE)
-#writeRaster(envi_fut.mask[[2]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_8', 
-#            format="GTiff", overwrite=TRUE)
-#writeRaster(envi_fut.mask[[3]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_15', 
-#            format="GTiff", overwrite=TRUE)
-#writeRaster(envi_fut.mask[[4]], filename='./data/env_sel/future/bc25_ssp126_81-00/bio_18', 
-#            format="GTiff", overwrite=TRUE)
-
-
-
-# Repeat this routine for other scenarios, GCMs, and years that interest you
